95ae76fe48a3438d66118a821c629cfb7d5c27e9
MeganClose <megs.close@gmail.com>
2021-06-23 12:25:17 -0700

    tests passing for reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,16 +1,23 @@
 import { useReducer } from "react";
+import { startSession } from "../../../server/config/connection";
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-} from "./actions";
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+} from './actions';
 
 export const reducer = (state, action) => {
     switch (action.type) {
         // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
         case UPDATE_PRODUCTS:
             return {
-                ...state, 
+                ...state,
                 products: [...action.products],
             };
         // if action type values is `UPDATE_CATEGORIES
@@ -26,9 +33,66 @@ export const reducer = (state, action) => {
                 ...state,
                 currentCategory: action.currentCategory
             };
-            // if it's none of these actions, do not update state at all and keep things the same
-            default: 
-                return state;
+
+        // if action type is "ADD_TO_CART"
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+
+        // if action type is "ADD_MULTIPLE_TO_CART"
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+
+            };
+
+        // if action type is " REMOVE_FROM_CART"
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            }
+
+        // if the action type is "UPDATE_CART_QUANTITY"
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                })
+            };
+
+        // if the action type is "CEAR_CART"
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+
+        // if the action type is "TOGGLE_CART"
+        case TOGGLE_CART: 
+           return {
+               ...state,
+               cartOpen: !state.cartOpen
+           };
+
+        // if it's none of these actions, do not update state at all and keep things the same
+        default:
+            return state;
     }
 };
 
